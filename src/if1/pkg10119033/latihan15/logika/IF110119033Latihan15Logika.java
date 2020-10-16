f3d026087de79736b0d35a547e2ed6699bd1dbb1
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2020-10-16 10:58:38 +0700

    Initial Commit

---
+++ src/if1/pkg10119033/latihan15/logika/IF110119033Latihan15Logika.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119033.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Dandi Ahmadin
+ * KELAS    : IF-1
+ * NIM      : 10119033
+ * Deskripsi Program : Program ini berisi operator logika
+ */
+public class IF110119033Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
